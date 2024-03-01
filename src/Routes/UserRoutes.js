c5e18d34ef2797691cd9e657014f369a2cf8a70f
SUMIT NIMESH <30859272+sumit1999nimesh@users.noreply.github.com>
2024-03-01 14:03:01 +0530

    Update UserRoutes.js

--- src/Routes/UserRoutes.js
+++ src/Routes/UserRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const auth = require("../Middleware/auth");
 const dotenv = require('dotenv');
-const { signup, login,updateUser } = require('../Controllers/userController');
+const { signup, login,updateUser,getUserName } = require('../Controllers/userController');
 
 const userRouter =express.Router();
 
 userRouter.post("/signup" ,signup);
 userRouter.post("/login" ,login);
 userRouter.put("/updateuser/" ,auth , updateUser);
+userRouter.get("/getUserName" ,auth , getUserName);
 
 module.exports= userRouter;
